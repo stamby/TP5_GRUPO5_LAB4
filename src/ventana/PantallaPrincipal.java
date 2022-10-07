e9daf0d0049ed11eb246d846b60b0c7967ba277d
unknown <bruno.molteni@alumnos.frgp.utn.edu.ar>
2022-10-06 23:09:46 -0300

    Primer commit

---
+++ src/ventana/PantallaPrincipal.java
@@ -0,0 +1,38 @@
+package ventana;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class PantallaPrincipal extends JFrame {
+
+	private JPanel contentPane;
+
+	public PantallaPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnPeliculas = new JMenu("Peliculas");
+		menuBar.add(mnPeliculas);
+		
+		JMenuItem mntmAgregar = new JMenuItem("Agregar");
+		mnPeliculas.add(mntmAgregar);
+		
+		JMenuItem mntmListar = new JMenuItem("Listar");
+		mnPeliculas.add(mntmListar);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ src/ventana/Principal.java
@@ -0,0 +1,10 @@
+package ventana;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		PantallaPrincipal x = new PantallaPrincipal();
+		x.setVisible(true);
+	}
+
+}
